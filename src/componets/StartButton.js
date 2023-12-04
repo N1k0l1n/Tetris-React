b0d2a315003f57f29f0618d3d3f0bf2bf0fe8ba4
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-12-04 11:24:40 +0100

    Adding Controls Buttons

--- src/componets/StartButton.js
+++ src/componets/StartButton.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { StyledStartButton } from './styles/StyledStartButton'
 
 const StartButton = ({callback}) => {
   return (
-    <div>StartButton</div>
+    <StyledStartButton onClick={callback}>StartButton</StyledStartButton>
   )
 }
 
